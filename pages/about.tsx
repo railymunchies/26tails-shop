a589b0088372df0214cf8eb412dfed4453dec784
railymunchies <railymunchies@gmail.com>
2025-06-27 13:11:17 +0700

    Create about.tsx

---
+++ pages/about.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function AboutPage() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>About 26tails.shop</h1>
+      <p>
+        26tails.shop is a lovingly crafted online store for rescued cats.
+        We offer premium cat food that helps support 26 adorable rescued companions!
+      </p>
+      <p>
+        This website is built using Next.js + TypeScript and deployed on Vercel.
+      </p>
+    </div>
+  );
+}Add About page
